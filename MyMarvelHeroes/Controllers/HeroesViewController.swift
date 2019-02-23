9a2a075273eb5b0dd529873bd96999780af011b3
Matheus Lima <matheus.lima@lavid.ufpb.br>
2019-02-23 14:46:51 -0300

    feat: Assets and controllers

---
+++ MyMarvelHeroes/Controllers/HeroesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeroesViewController.swift
+//  MyMarvelHeroes
+//
+//  Created by Matheus Lima on 18/02/19.
+//  Copyright © 2019 Matheus Lima. All rights reserved.
+//
+
+import UIKit
+
+class HeroesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
